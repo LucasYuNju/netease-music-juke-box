48d600c57a0623bbcc6c558549e7856f5b341ab5
LucasYuNju <LucasYuNju@gmail.com>
2016-07-18 10:06:33 +0800

    add more element to player view.

--- src/nm/view/PlayerView.js
+++ src/nm/view/PlayerView.js
@@ -4,9 +4,21 @@ export default class PlayListView extends View {
     init() {
         super.init();
         this._track = null;
+        this._playing = false;
+        this._currentTime = 0;
+        this.addStyleClass("nm-player-view");
         this.$audio = $(`<audio controls autoplay loop/>`);
         this.$element.append(this.$audio);
-        this.addStyleClass("nm-player-view");
+        this.$play = $(`<span class="player-control">播放</span>`);
+        this.$element.append(this.$play);
+        this.$next = $(`<span class="player-control">下一首</span>`);
+        this.$element.append(this.$next);
+        this.$progressBar = $(`<span class="player-control">00:00</span>`);
+        this.$element.append(this.$progressBar);
+
+        this.$audio[0].addEventListener("progress", this._audio_progress.bind(this));
+        this.$play.on("click", this._play_click.bind(this));
+        this.$progressBar.on("click", this._progressBar_click.bind(this));
     }
 
     getElementTag() {
@@ -25,8 +37,22 @@ export default class PlayListView extends View {
     }
 
     renderPlayer() {
-        console.log(this.track);
+        // console.log(this.track);
         const mp3 = this.track.mp3Url;
         this.$audio.attr("src", mp3);
+        this._playing = true;
+    }
+
+    _play_click(e) {
+        this._playing = !this._playing;
+        this.$play.text(this._playing ? "暂停" : "播放");
+    }
+
+    _progressBar_click(e) {
+
+    }
+
+    _audio_progress(e) {
+        console.log("progress");
     }
 }
